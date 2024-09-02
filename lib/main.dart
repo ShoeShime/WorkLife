30fd1c5c089d29149312d43bd6bb05fa25e2da1a
Shime01 <evohflame71@gmail.com>
2024-09-02 05:58:06 -0700

    Adding in a features folder and the name of specific features I may work on in the future.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-//import 'package:intl/intl.dart';
+import 'package:intl/intl.dart';
+import "features/TimeRemaining/TimeRemaining.dart";
 
 void main() {
   runApp(const WorkLifeApp());
@@ -16,72 +17,6 @@ class WorkLifeApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  String _hoursLeft = '';
-
-  @override
-  void initState() {
-    super.initState();
-    _calculateHoursLeft();
-    _startTimer();
-  }
-
-  void _startTimer() {
-    // Update every hour
-    Future.delayed(const Duration(hours: 1), () {
-      _calculateHoursLeft();
-      _startTimer();  // restart the timer
-    });
-  }
-
-  void _calculateHoursLeft() {
-    final now = DateTime.now();
-    final endOfWeek = DateTime(now.year, now.month, now.day, 23, 59, 59)
-        .add(Duration(days: DateTime.daysPerWeek - now.weekday));
-    final hoursLeft = endOfWeek.difference(now).inHours;
-
-    setState(() {
-      _hoursLeft = hoursLeft.toString();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Hours Left in the Week'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Hours left in the week:',
-              style: TextStyle(fontSize: 24),
-            ),
-            const SizedBox(height: 20),
-            CircleAvatar(
-              radius: 150,
-              backgroundColor: Colors.blue.withOpacity(0.7),
-              child: Text(
-                _hoursLeft,
-                style: const TextStyle(fontSize: 48, color: Colors.white),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
